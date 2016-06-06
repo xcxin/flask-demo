fa9bee2e5ff5fa76993564f654b7df865c125046
xcxin <xcshin@gmail.com>
2016-06-05 22:32:28 -0500

    D

--- app.py
+++ app.py
@@ -18,19 +18,20 @@ def index():
   else:
 	app.vars['ticker']=request.form['ticker_lulu']
 	
-	#return render_template('index.html',question='input stock ticker')
+	#return render_template('index.html')
 	return redirect ('/result')
 
-@app.route('/result')
+@app.route('/result',methods=['GET'])
 def result():
-  data=json.load(urllib2.urlopen('https://www.quandl.com/api/v3/datasets/WIKI/'+app.vars['ticker']+'.json?start_date=2016-05-03&end_date=2016-06-03&column_index=4'))
-  mylist=[0 for i in range(23)]
-  for i in range(23):
-	mylist[i]=data['dataset']['data'][i][1]	
+  #data=json.load(urllib2.urlopen#('https://www.quandl.com/api/v3/datasets/WIKI/'+app.vars['ticker']+'.json?#start_date=2016-05-03&end_date=2016-06-03&column_index=4'))
+  #mylist=[0 for i in range(23)]
+  #for i in range(23):
+#	mylist[i]=data['dataset']['data'][i][1]	
   
-  p=figure(plot_width=400,plot_height=400)
-  p.line(range(23),mylist,line_width=2)
-  return p
+  #p=figure(plot_width=400,plot_height=400)
+  #p.line(range(23),mylist,line_width=2)
+  #return p
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(port=33507,debug=True)
